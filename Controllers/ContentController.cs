16946abb2ff7f2275bc2647720afb29fdfed888f
Parsonious <135645826+Parsonious@users.noreply.github.com>
2024-09-03 17:20:57 -0400

    add new class to handle pulling and parsing html for css and js files add new dependency for this new class

--- Controllers/ContentController.cs
+++ Controllers/ContentController.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 
+
 namespace ZD_Article_Grabber
 {
-    [Route("api/[controller]")]
+    [Route("a/p")]
     [ApiController]
     public class ContentController : ControllerBase
     {
@@ -18,7 +19,7 @@ namespace ZD_Article_Grabber
 
         [HttpGet("{title}")]
 
-        public IActionResult GetContent(string title)
+        public async Task<IActionResult> GetContent(string title)
         {
             // Normalize title to match your file naming convention
             string normalizedTitle = title.Replace(" ", "%20");
@@ -26,30 +27,26 @@ namespace ZD_Article_Grabber
 
 
             if(!_cache.TryGetValue(cacheKey, out string htmlContent))
-            {
-                // Cache not found => Fetch from external source
-                string url = $"https://parsonious.github.io/CIQ-How-To/pages/{normalizedTitle}.html";
-                using(var client = new HttpClient() )
-                {
-                    // Determine result from client
-                    var response = client.GetAsync(url).Result;
-                    if(response.IsSuccessStatusCode)
-                    {
-                        htmlContent = response.Content.ReadAsStringAsync().Result;
+            {  // Cache not found => Fetch from external source
+                Fetch fetcher = new Fetch();
 
-                        // Set cache options
-                        var cacheEntryOptions = new MemoryCacheEntryOptions()
-                            .SetSlidingExpiration(TimeSpan.FromMinutes(10)); //adjust for cache duration
+                string url = $"https://parsonious.github.io/CIQ-How-To/pages/{normalizedTitle}.html";
+                 htmlContent = await fetcher.FetchAndModifyHtmlAsync(url);
+                
+                
+                // Set cache options
+               var cacheEntryOptions = new MemoryCacheEntryOptions()
+               .SetSlidingExpiration(TimeSpan.FromMinutes(10)); //adjust for cache duration
                         
-                        // Save data in cache
-                        _cache.Set(cacheKey, htmlContent, cacheEntryOptions);
-                    }
-                    else
-                    {
-                        return NotFound("Page not found");
-                    }
-                }
+               // Save data in cache
+               _cache.Set(cacheKey, htmlContent, cacheEntryOptions);
             }
+            else
+            {
+               return NotFound("Page not found");
+            }
+               
+           
             return Content(htmlContent, "text/html");
            
         }
