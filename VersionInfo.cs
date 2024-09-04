42e54f52b03f2938234192293de1615049827c75
Parsonious <135645826+Parsonious@users.noreply.github.com>
2024-09-04 16:44:06 -0400

    update version info and get (kind of original) contentController back

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string Major = "1";
         public const string Minor = "2";
-        public const string Patch = "0";//patch
+        public const string Patch = "1";
     }
 }
