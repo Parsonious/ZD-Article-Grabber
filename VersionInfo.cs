fa9766f60e90e2505a2ef494dba2a3dafbfa6479
Parsonious <135645826+Parsonious@users.noreply.github.com>
2024-09-05 17:51:04 -0400

    - updated versioning. - adding missing service

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string Major = "1";
         public const string Minor = "3";
-        public const string Patch = "2";
+        public const string Patch = "4";
     }
 }
