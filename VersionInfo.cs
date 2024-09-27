fd63f02ee259cd2ee22d7d135310bb798f03f0e5
Parsonious <135645826+Parsonious@users.noreply.github.com>
2024-09-27 16:17:56 -0400

    version info update.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -3,7 +3,7 @@
     public class VersionInfo
     {
         public const string Major = "1";
-        public const string Minor = "5";
+        public const string Minor = "6";
         public const string Patch = "4";
     }
 }
