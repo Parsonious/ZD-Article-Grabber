92be4551393e85976a18736256e712e9840b2ba8
Parsonious <135645826+Parsonious@users.noreply.github.com>
2024-10-28 16:58:53 -0400

    update version and added volume attachment to the dev deploy yml.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string Major = "1";
         public const string Minor = "7";
-        public const string Patch = "1";
+        public const string Patch = "2";
     }
 }
