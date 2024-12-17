5185b81069318d3adb7c2f5a9582d87435f112f3
Parsonious <135645826+Parsonious@users.noreply.github.com>
2024-12-17 11:59:10 -0500

    2.3.1

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string Major = "2";
         public const string Minor = "3";
-        public const string Patch = "0";
+        public const string Patch = "1";
     }
 }
