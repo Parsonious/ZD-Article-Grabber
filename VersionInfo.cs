4bc8f8b1ec114fab661cf33427fc24af6098afc6
Parsonious <135645826+Parsonious@users.noreply.github.com>
2025-03-07 16:51:23 -0500

    update version

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,9 +2,8 @@
 {
     public class VersionInfo
     {
-        public const string Major = "2";
-        public const string Minor = "5";
-        public const string Patch = "2";
+        public const string Major = "3";
+        public const string Minor = "0";
+        public const string Patch = "0";
     }
 }
-//due to the name of this branch, it will not auto deploy to the dev environment.
